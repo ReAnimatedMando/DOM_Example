905a8a13e222b883d6a3d5ea0a0093d47e9ab4f1
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2024-07-29 10:46:37 -0600

    dom ex
    fixed bug in js preventing changes from taking effect

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let list = document.getElementsByTagName('ul')[0];  // Get the <ul> element
 
 // ADD NEW ITEM TO END OF LIST
-let newItemLast = document.createdElement('li');    // Create element
+let newItemLast = document.createElement('li');    // Create element
 let newTextLast = document.createTextNode('cream'); // Create text node
 newItemLast.appendChild(newTextLast);               // Add text node to element
 list.appendChild(newItemLast);                      // Add element end of list
